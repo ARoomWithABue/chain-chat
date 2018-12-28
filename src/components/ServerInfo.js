d8bc5ae19b141f7d3906a7d3489f91eb68225f54
Ed Silva <eed.silva@hotmail.com>
2018-12-28 11:32:25 -0800

    using props instead of state on child components

--- src/components/ServerInfo.js
+++ src/components/ServerInfo.js
@@ -5,7 +5,6 @@ class ServerInfo extends Component {
         super(props);
 
         this.state = {
-            user: props.user,
             channels: []
         }
     }
@@ -25,14 +24,14 @@ class ServerInfo extends Component {
 
     render() {
         let userStyle = {
-            backgroundImage: `url("${this.state.user.image}")`,
+            backgroundImage: `url("${this.props.user.image}")`,
         };
         return (
           <div id="Server-Info" className="flexColumn">
               <div id="User-Profile" className="row flexFit">
                   <div className="col-3"><div className="userIcon-large shadow" style={userStyle}></div></div>
                   <div className="col-1"></div>
-                  <div className="col-7 top7"><span className="userName">{this.state.user.profileName}</span></div>
+                  <div className="col-7 top7"><span className="userName">{this.props.user.profileName}</span></div>
               </div>
               <div id="Server-Channels">
 
